ea58f96ec05c0569a5eb9801a641890a9b5732a7
Marlon <reinerbutzbach@gmail.com>
2026-05-21 01:46:42 +0200

    connect webapp buttons to command api

--- server.js
+++ server.js
@@ -38,20 +38,7 @@ app.get("/api/calibrate", (req, res) => {
 
   res.json({ ok: true, command: currentCommand });
 });
-async function enableClockMode(){
-  const res = await fetch("/api/clock");
-  const data = await res.json();
-  console.log("Uhrmodus aktiviert:", data);
 
-  safeText(displayBadge, "Clock mode");
-  safeClass(displayBadge, "badge green");
-}
-
-async function enableManualMode(){
-  const res = await fetch("/api/manual");
-  const data = await res.json();
-  console.log("Manuell aktiviert:", data);
-}
 
 app.get("/api/command", (req, res) => {
   res.json(currentCommand);
